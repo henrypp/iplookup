a72c148b928163b9ccf7f75884b820f7f5abe7fb
henrypp <sforce5@mail.ru>
2016-01-22 02:19:06 +0600

    v1.4 released.

--- bin/Readme.txt
+++ bin/Readme.txt
@@ -1,12 +1,10 @@
-##Ip Lookup
+Ip Lookup
 
 Application for displaying list of IP addresses your computer, include external address.
 
-```
 To activate portable mode, create "iplookup.ini" in application folder, or move it from "%APPDATA%\Henry++\Ip Lookup".
-```
 
-Website: www.henrypp.org<br />
-Support: support@henrypp.org<br />
-<br />
+Website: www.henrypp.org
+Support: support@henrypp.org
+
 (c) 2011-2016 Henry++
--- src/resource.h
+++ src/resource.h
@@ -5,46 +5,55 @@
 #define IDC_STATIC (-1)
 #endif
 
-// Dialogs
-#define IDD_MAIN	                            100
+// Menu Id
+#define IDM_MAIN 100
+#define IDM_LISTVIEW 101
 
-// Menus
-#define IDM_MAIN		                        100
+// Dialogs
+#define IDD_MAIN 100
 
 // Main Dlg
-#define IDC_IP_LIST								100
-#define IDC_EXTERNAL_IP_CHK						101
-#define IDC_REFRESH								102
-#define IDC_EXIT								103
+#define IDC_LISTVIEW 100
+#define IDC_REFRESH 101
+#define IDC_EXIT 102
+
+#define IDC_EXTERNAL_IP_CHK 201
 
 // Main Menu
-#define IDM_EXIT                                40000
-#define IDM_CHECKUPDATEATSTARTUP_CHK			40001
-#define IDM_ALWAYSONTOP_CHK						40002
-#define IDM_GETATSTARTUP_CHK					40003
-#define IDM_WEBSITE                             40004
-#define IDM_CHECK_UPDATES		                40005
-#define IDM_ABOUT                               40006
+#define IDM_EXIT 2000
+#define IDM_CHECKUPDATES_CHK 2001
+#define IDM_GETEXTERNALIP_CHK 2003
+#define IDM_WEBSITE 2004
+#define IDM_DONATE 2005
+#define IDM_CHECKUPDATES 2006
+#define IDM_ABOUT 2007
+
+// Listview Menu
+#define IDM_COPY 3000
 
 // Strings
-#define IDS_TRANSLATION_INFO					1000
+#define IDS_UPDATE_NO 1000
+#define IDS_UPDATE_YES 1001
 
-#define IDS_UPDATE_NO                           10000
-#define IDS_UPDATE_YES                          10001
-#define IDS_UPDATE_ERROR                        10002
-#define IDS_UPDATE_ERROR                        10002
+#define IDS_FILE 1002
+#define IDS_EXIT 1003
+#define IDS_SETTINGS 1004
+#define IDS_CHECKUPDATES_CHK 1005
+#define IDS_GETEXTERNALIP_CHK 1006
+#define IDS_HELP 1007
+#define IDS_WEBSITE 1008
+#define IDS_DONATE 1009
+#define IDS_CHECKUPDATES 1010
+#define IDS_ABOUT 1011
 
-#define IDS_ABOUT								10003
-#define IDS_COPYRIGHT							10004
+#define IDS_COPY 1012
 
-#define IDS_IP_LOADING							10005
-#define IDS_IP_ERROR							10006
-#define IDS_IP_LOCAL							10007
-#define IDS_IP_EXTERNAL							10008
+#define IDS_REFRESH 1013
 
-#define IDS_WINSOCK_ERROR						10009
+#define IDS_IPLIST1 1014
+#define IDS_IPLIST2 1015
 
 // Icons
-#define IDI_MAIN	                            100
+#define IDI_MAIN 100
 
 #endif
