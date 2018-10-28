29eeea61a8a9abc3434122ae1f563718845494c5
henrypp <support@henrypp.org>
2018-10-28 14:59:57 +0600

    upd code

---
+++ src/main.hpp
@@ -0,0 +1,19 @@
+// Ip Lookup
+// Copyright (c) 2011-2018 Henry++
+
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <windows.h>
+#include <commctrl.h>
+
+#include "resource.hpp"
+#include "app.hpp"
+
+// libs
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
+
+#define EXTERNAL_URL L"https://api.ipify.org/?format=text" 
+
+#endif // __MAIN_H__
