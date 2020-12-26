019023f5366322d4aa308e858fab0ba3da6af81b
henrypp <support@henrypp.org>
2020-12-26 19:37:15 +0600

    rename headers

---
+++ src/app.h
@@ -0,0 +1,11 @@
+// // Ip Lookup
+// Copyright (c) 2011-2021 Henry++
+
+#pragma once
+
+#define APP_NAME L"Ip Lookup"
+#define APP_NAME_SHORT L"iplookup"
+#define APP_VERSION L"1.6.1"
+#define APP_VERSION_RES 1,6,1,0
+#define APP_AUTHOR L"Henry++"
+#define APP_COPYRIGHT L"(c) 2011-2021 " APP_AUTHOR L". All Rights Reserved."
---
+++ src/main.h
@@ -0,0 +1,17 @@
+// Ip Lookup
+// Copyright (c) 2011-2021 Henry++
+
+#pragma once
+
+#include "routine.h"
+
+#include "resource.h"
+#include "app.h"
+
+// libs
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
+
+#define LANG_MENU 6
+
+#define EXTERNAL_URL L"https://api.ipify.org/?format=text" 
