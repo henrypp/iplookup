199e45682aec0574b80a88031e09555cdaec3df1
henrypp <support@henrypp.org>
2021-04-08 18:18:22 +0600

    upd build script

--- build_resources.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-php "..\builder\make_resource.php" ".\src\resource.h"
-php "..\builder\make_locale.php" "Ip Lookup" "iplookup" ".\bin\i18n" ".\src\resource.h" ".\src\resource.rc" ".\bin\iplookup.lng"
-copy /y ".\bin\iplookup.lng" ".\bin\32\iplookup.lng"
-copy /y ".\bin\iplookup.lng" ".\bin\64\iplookup.lng"
-
-pause
