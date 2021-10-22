eb48291c7bb00ae2a0eea94095b9fb18c32cdfc5
henrypp <support@henrypp.org>
2021-10-23 03:33:54 +0600

    upd routine

--- src/main.c
+++ src/main.c
@@ -13,27 +13,35 @@
 
 #include "resource.h"
 
-R_SPINLOCK lock_thread;
+R_QUEUED_LOCK lock_thread = PR_QUEUED_LOCK_INIT;
 
-THREAD_API _app_print (PVOID lparam)
+NTSTATUS NTAPI _app_print (PVOID lparam)
 {
 	HWND hwnd = (HWND)lparam;
 	WSADATA wsa;
 	WCHAR buffer[128];
 	PIP_ADAPTER_ADDRESSES adapter_addresses;
 	PIP_ADAPTER_ADDRESSES adapter;
-	ULONG size = 0;
+	ULONG size;
 	ULONG code;
 	INT item_id;
 
-	_r_spinlock_acquireshared (&lock_thread);
+	R_DOWNLOAD_INFO download_info;
+	HINTERNET hsession;
+	PR_STRING url_string;
+
+	_r_queuedlock_acquireshared (&lock_thread);
 
 	_r_status_settext (hwnd, IDC_STATUSBAR, 0, L"Loading....");
 
 	_r_listview_deleteallitems (hwnd, IDC_LISTVIEW);
 
+	item_id = 0;
+
 	if (WSAStartup (WINSOCK_VERSION, &wsa) == ERROR_SUCCESS)
 	{
+		size = 0;
+
 		while (TRUE)
 		{
 			size += 1024;
@@ -75,8 +83,10 @@ THREAD_API _app_print (PVOID lparam)
 
 						if (InetNtop (af, &(ipv4->sin_addr), buffer, RTL_NUMBER_OF (buffer)))
 						{
-							item_id = _r_listview_additemex (hwnd, IDC_LISTVIEW, -1, 0, buffer, I_IMAGENONE, 0, 0);
+							_r_listview_additem_ex (hwnd, IDC_LISTVIEW, item_id, buffer, I_IMAGENONE, 0, 0);
 							_r_listview_setitem (hwnd, IDC_LISTVIEW, item_id, 1, adapter->Description);
+
+							item_id += 1;
 						}
 					}
 					else if (af == AF_INET6)
@@ -86,8 +96,10 @@ THREAD_API _app_print (PVOID lparam)
 
 						if (InetNtop (af, &(ipv6->sin6_addr), buffer, RTL_NUMBER_OF (buffer)))
 						{
-							item_id = _r_listview_additemex (hwnd, IDC_LISTVIEW, -1, 0, buffer, I_IMAGENONE, 1, 0);
+							_r_listview_additem_ex (hwnd, IDC_LISTVIEW, item_id, buffer, I_IMAGENONE, 1, 0);
 							_r_listview_setitem (hwnd, IDC_LISTVIEW, item_id, 1, adapter->Description);
+
+							item_id += 1;
 						}
 					}
 				}
@@ -101,29 +113,30 @@ THREAD_API _app_print (PVOID lparam)
 
 	if (_r_config_getboolean (L"GetExternalIp", FALSE))
 	{
-		LPCWSTR url_string = _r_config_getstring (L"ExternalUrl", EXTERNAL_URL);
+		url_string = _r_config_getstring (L"ExternalUrl", EXTERNAL_URL);
 
 		if (url_string)
 		{
-			R_DOWNLOAD_INFO download_info;
-			HINTERNET hsession;
-
 			hsession = _r_inet_createsession (_r_app_getuseragent ());
 
 			if (hsession)
 			{
 				_r_inet_initializedownload (&download_info, NULL, NULL, NULL);
 
-				if (_r_inet_begindownload (hsession, url_string, &download_info) == ERROR_SUCCESS)
+				if (_r_inet_begindownload (hsession, url_string->buffer, &download_info) == ERROR_SUCCESS)
 				{
-					item_id = _r_listview_additemex (hwnd, IDC_LISTVIEW, -1, 0, _r_obj_getstringorempty (download_info.string), I_IMAGENONE, 2, 0);
-					_r_listview_setitem (hwnd, IDC_LISTVIEW, item_id, 1, url_string);
+					_r_listview_additem_ex (hwnd, IDC_LISTVIEW, item_id, _r_obj_getstringorempty (download_info.string), I_IMAGENONE, 2, 0);
+					_r_listview_setitem (hwnd, IDC_LISTVIEW, item_id, 1, url_string->buffer);
+
+					item_id += 1;
 				}
 
 				_r_inet_destroydownload (&download_info);
 
 				_r_inet_close (hsession);
 			}
+
+			_r_obj_dereference (url_string);
 		}
 	}
 
@@ -132,7 +145,7 @@ THREAD_API _app_print (PVOID lparam)
 
 	_r_status_settextformat (hwnd, IDC_STATUSBAR, 0, _r_locale_getstring (IDS_STATUS), _r_listview_getitemcount (hwnd, IDC_LISTVIEW));
 
-	_r_spinlock_releaseshared (&lock_thread);
+	_r_queuedlock_releaseshared (&lock_thread);
 
 	return ERROR_SUCCESS;
 }
@@ -145,13 +158,15 @@ INT_PTR CALLBACK DlgProc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 	{
 		case WM_INITDIALOG:
 		{
+			UINT state_mask;
+
 			// configure listview
 			_r_listview_setstyle (hwnd, IDC_LISTVIEW, LVS_EX_DOUBLEBUFFER | LVS_EX_FULLROWSELECT | LVS_EX_INFOTIP | LVS_EX_LABELTIP, TRUE);
 
 			_r_listview_addcolumn (hwnd, IDC_LISTVIEW, 0, NULL, 10, LVCFMT_LEFT);
 			_r_listview_addcolumn (hwnd, IDC_LISTVIEW, 1, NULL, 10, LVCFMT_LEFT);
 
-			UINT state_mask = 0;
+			state_mask = 0;
 
 			if (_r_sys_isosversiongreaterorequal (WINDOWS_VISTA))
 				state_mask = LVGS_COLLAPSIBLE;
@@ -159,8 +174,6 @@ INT_PTR CALLBACK DlgProc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 			_r_listview_addgroup (hwnd, IDC_LISTVIEW, 0, L"", 0, state_mask, state_mask);
 			_r_listview_addgroup (hwnd, IDC_LISTVIEW, 1, L"", 0, state_mask, state_mask);
 			_r_listview_addgroup (hwnd, IDC_LISTVIEW, 2, L"", 0, state_mask, state_mask);
-
-			_r_spinlock_initialize (&lock_thread);
 
 			_r_layout_initializemanager (&layout_manager, hwnd);
 
@@ -173,12 +186,13 @@ INT_PTR CALLBACK DlgProc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 		case RM_INITIALIZE:
 		{
 			// configure menu
-			HMENU hmenu = GetMenu (hwnd);
+			HMENU hmenu;
+
+			hmenu = GetMenu (hwnd);
 
 			if (hmenu)
 			{
 				CheckMenuItem (hmenu, IDM_ALWAYSONTOP_CHK, MF_BYCOMMAND | (_r_config_getboolean (L"AlwaysOnTop", FALSE) ? MF_CHECKED : MF_UNCHECKED));
-				CheckMenuItem (hmenu, IDM_CLASSICUI_CHK, MF_BYCOMMAND | (_r_config_getboolean (L"ClassicUI", APP_CLASSICUI) ? MF_CHECKED : MF_UNCHECKED));
 				CheckMenuItem (hmenu, IDM_GETEXTERNALIP_CHK, MF_BYCOMMAND | (_r_config_getboolean (L"GetExternalIp", FALSE) ? MF_CHECKED : MF_UNCHECKED));
 			}
 
@@ -188,7 +202,9 @@ INT_PTR CALLBACK DlgProc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 		case RM_LOCALIZE:
 		{
 			// localize
-			HMENU hmenu = GetMenu (hwnd);
+			HMENU hmenu;
+
+			hmenu = GetMenu (hwnd);
 
 			if (hmenu)
 			{
@@ -199,7 +215,6 @@ INT_PTR CALLBACK DlgProc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 
 				_r_menu_setitemtextformat (hmenu, IDM_EXIT, FALSE, L"%s\tEsc", _r_locale_getstring (IDS_EXIT));
 				_r_menu_setitemtext (hmenu, IDM_ALWAYSONTOP_CHK, FALSE, _r_locale_getstring (IDS_ALWAYSONTOP_CHK));
-				_r_menu_setitemtextformat (hmenu, IDM_CLASSICUI_CHK, FALSE, L"%s*", _r_locale_getstring (IDS_CLASSICUI_CHK));
 				_r_menu_setitemtext (hmenu, IDM_GETEXTERNALIP_CHK, FALSE, _r_locale_getstring (IDS_GETEXTERNALIP_CHK));
 				_r_menu_setitemtext (hmenu, IDM_WEBSITE, FALSE, _r_locale_getstring (IDS_WEBSITE));
 				_r_menu_setitemtext (hmenu, IDM_ABOUT, FALSE, _r_locale_getstring (IDS_ABOUT));
@@ -240,25 +255,35 @@ INT_PTR CALLBACK DlgProc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 
 		case WM_CONTEXTMENU:
 		{
+			HMENU hmenu;
+			HMENU hsubmenu;
+
 			if (GetDlgCtrlID ((HWND)wparam) != IDC_LISTVIEW)
 				break;
 
-			HMENU hmenu = LoadMenu (NULL, MAKEINTRESOURCE (IDM_LISTVIEW));
-			HMENU hsubmenu = GetSubMenu (hmenu, 0);
+			hmenu = LoadMenu (NULL, MAKEINTRESOURCE (IDM_LISTVIEW));
 
-			// localize
-			_r_menu_setitemtextformat (hmenu, IDM_REFRESH, FALSE, L"%s\tF5", _r_locale_getstring (IDS_REFRESH));
-			_r_menu_setitemtextformat (hmenu, IDM_COPY, FALSE, L"%s\tCtrl+C", _r_locale_getstring (IDS_COPY));
+			if (hmenu)
+			{
+				hsubmenu = GetSubMenu (hmenu, 0);
 
-			if (_r_spinlock_islocked (&lock_thread))
-				_r_menu_enableitem (hsubmenu, IDM_REFRESH, MF_BYCOMMAND, FALSE);
+				if (hsubmenu)
+				{
+					// localize
+					_r_menu_setitemtextformat (hmenu, IDM_REFRESH, FALSE, L"%s\tF5", _r_locale_getstring (IDS_REFRESH));
+					_r_menu_setitemtextformat (hmenu, IDM_COPY, FALSE, L"%s\tCtrl+C", _r_locale_getstring (IDS_COPY));
 
-			if (!_r_listview_getselectedcount (hwnd, IDC_LISTVIEW))
-				_r_menu_enableitem (hsubmenu, IDM_COPY, MF_BYCOMMAND, FALSE);
+					if (_r_queuedlock_islocked (&lock_thread))
+						_r_menu_enableitem (hsubmenu, IDM_REFRESH, MF_BYCOMMAND, FALSE);
 
-			_r_menu_popup (hsubmenu, hwnd, NULL, TRUE);
+					if (!_r_listview_getselectedcount (hwnd, IDC_LISTVIEW))
+						_r_menu_enableitem (hsubmenu, IDM_COPY, MF_BYCOMMAND, FALSE);
 
-			DestroyMenu (hmenu);
+					_r_menu_popup (hsubmenu, hwnd, NULL, TRUE);
+				}
+
+				DestroyMenu (hmenu);
+			}
 
 			break;
 		}
@@ -267,7 +292,7 @@ INT_PTR CALLBACK DlgProc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 		{
 			if (HIWORD (wparam) == 0 && LOWORD (wparam) >= IDX_LANGUAGE && LOWORD (wparam) <= IDX_LANGUAGE + _r_locale_getcount ())
 			{
-				_r_locale_applyfrommenu (GetSubMenu (GetSubMenu (GetMenu (hwnd), 1), LANG_MENU), LOWORD (wparam));
+				_r_locale_apply (GetSubMenu (GetSubMenu (GetMenu (hwnd), 1), LANG_MENU), LOWORD (wparam), IDX_LANGUAGE);
 
 				return FALSE;
 			}
@@ -289,18 +314,6 @@ INT_PTR CALLBACK DlgProc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 					_r_config_setboolean (L"AlwaysOnTop", new_val);
 
 					_r_wnd_top (hwnd, new_val);
-
-					break;
-				}
-
-				case IDM_CLASSICUI_CHK:
-				{
-					BOOLEAN new_val = !_r_config_getboolean (L"ClassicUI", APP_CLASSICUI);
-
-					CheckMenuItem (GetMenu (hwnd), IDM_CLASSICUI_CHK, MF_BYCOMMAND | (new_val ? MF_CHECKED : MF_UNCHECKED));
-					_r_config_setboolean (L"ClassicUI", new_val);
-
-					_r_app_restart (hwnd);
 
 					break;
 				}
@@ -331,7 +344,7 @@ INT_PTR CALLBACK DlgProc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 
 				case IDM_REFRESH:
 				{
-					if (!_r_spinlock_islocked (&lock_thread))
+					if (!_r_queuedlock_islocked (&lock_thread))
 						_r_sys_createthread2 (&_app_print, hwnd);
 
 					break;
@@ -341,30 +354,30 @@ INT_PTR CALLBACK DlgProc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 				{
 					R_STRINGBUILDER buffer;
 					PR_STRING string;
+					INT item_id;
+
+					item_id = -1;
 
 					_r_obj_initializestringbuilder (&buffer);
 
-					INT item = -1;
-
-					while ((item = (INT)SendDlgItemMessage (hwnd, IDC_LISTVIEW, LVM_GETNEXTITEM, item, LVNI_SELECTED)) != -1)
+					while ((item_id = (INT)SendDlgItemMessage (hwnd, IDC_LISTVIEW, LVM_GETNEXTITEM, item_id, LVNI_SELECTED)) != -1)
 					{
-						string = _r_listview_getitemtext (hwnd, IDC_LISTVIEW, item, 0);
+						string = _r_listview_getitemtext (hwnd, IDC_LISTVIEW, item_id, 0);
 
 						if (string)
 						{
-							_r_obj_appendstringbuilderformat (&buffer, L"%s\r\n", string->buffer);
+							_r_obj_appendstringbuilder2 (&buffer, string);
+							_r_obj_appendstringbuilder (&buffer, L"\r\n");
+
 							_r_obj_dereference (string);
 						}
 					}
 
 					string = _r_obj_finalstringbuilder (&buffer);
 
-					if (!_r_obj_isstringempty (string))
-					{
-						_r_obj_trimstring (string, L"\r\n");
+					_r_str_trimstring2 (string, L"\r\n ", 0);
 
-						_r_clipboard_set (hwnd, string->buffer, _r_obj_getstringlength (string));
-					}
+					_r_clipboard_set (hwnd, &string->sr);
 
 					_r_obj_deletestringbuilder (&buffer);
 
@@ -388,19 +401,21 @@ INT_PTR CALLBACK DlgProc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
 INT APIENTRY wWinMain (_In_ HINSTANCE hinst, _In_opt_ HINSTANCE prev_hinst, _In_ LPWSTR cmdline, _In_ INT show_cmd)
 {
 	MSG msg;
+	HACCEL haccel;
+	HWND hwnd;
 
 	if (_r_app_initialize ())
 	{
-		if (_r_app_createwindow (IDD_MAIN, IDI_MAIN, &DlgProc))
+		hwnd = _r_app_createwindow (IDD_MAIN, IDI_MAIN, &DlgProc);
+
+		if (hwnd)
 		{
-			HACCEL haccel = LoadAccelerators (_r_sys_getimagebase (), MAKEINTRESOURCE (IDA_MAIN));
+			haccel = LoadAccelerators (_r_sys_getimagebase (), MAKEINTRESOURCE (IDA_MAIN));
 
 			if (haccel)
 			{
 				while (GetMessage (&msg, NULL, 0, 0) > 0)
 				{
-					HWND hwnd = GetActiveWindow ();
-
 					if (!TranslateAccelerator (hwnd, haccel, &msg) && !IsDialogMessage (hwnd, &msg))
 					{
 						TranslateMessage (&msg);
