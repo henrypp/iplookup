1482923a19d4194d2069392c5f36ce1fdf3de57b
henrypp <support@henrypp.org>
2022-03-25 00:31:14 +0600

    upd app.h

--- src/app.h
+++ src/app.h
@@ -1,11 +1,11 @@
-// // iplookup
-// Copyright (c) 2011-2021 Henry++
+// iplookup
+// Copyright (c) 2011-2022 Henry++
 
 #pragma once
 
 #define APP_NAME L"iplookup"
 #define APP_NAME_SHORT L"iplookup"
-#define APP_VERSION L"1.7.3"
-#define APP_VERSION_RES 1,7,3,0
+#define APP_VERSION L"1.7.4"
+#define APP_VERSION_RES 1,7,4,0
 #define APP_AUTHOR L"Henry++"
-#define APP_COPYRIGHT L"(c) 2011-2021 " APP_AUTHOR L". All Rights Reserved."
+#define APP_COPYRIGHT L"(c) 2011-2022 " APP_AUTHOR L". All Rights Reserved."
